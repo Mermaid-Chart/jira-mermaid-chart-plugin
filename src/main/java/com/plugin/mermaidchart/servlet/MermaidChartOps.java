d81ccc0a31af303c6c329083bf64f3e907f9c8f1
Per Brolin <per@mermaidchart.com>
2023-07-10 11:22:28 +0200

    MC-666: Added settings link on top-navigation bar for now

--- src/main/java/com/plugin/mermaidchart/servlet/MermaidChartOps.java
+++ src/main/java/com/plugin/mermaidchart/servlet/MermaidChartOps.java
@@ -17,6 +17,7 @@ public class MermaidChartOps extends HttpServlet {
   @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
     throws ServletException, IOException {
+    // TODO: Replace with rendering of page and better styling
     resp.setContentType("text/html");
     resp
       .getWriter()
